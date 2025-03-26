9f4c5010e9c4bcc6cbe5594701ecbccfe4531c60
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-26 00:46:53 -0400

    chore: auth integration

--- src/workspaces/authorized.workspace.tsx
+++ src/workspaces/authorized.workspace.tsx
@@ -1,8 +1,22 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Navbar } from '@/components/ui/navbar';
+import { useAuth } from '@/hooks/auth/auth.hook';
 
 export function Authorized() {
+  const auth = useAuth()
+
   return (
-    <div>
-      <h1>Authorized</h1>
+    <div className="min-h-screen flex flex-col">
+      <Navbar auth={auth}/>
+      <main className="flex-1 container py-6">
+        <Routes>
+          <Route path="/" element={<div><h1>Authorized</h1></div>} />
+          <Route
+              path="/iniciar"
+              element={<Navigate to="/" replace />}
+          />
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/workspaces/guest.workspace.tsx
+++ src/workspaces/guest.workspace.tsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Login } from '@/modules/auth/login/login.view';
+import { Navbar } from '@/components/ui/navbar';
+
+function Scafold() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 container py-6">
+        <Routes>
+          <Route path="/" element={<div><h1>Guest</h1></div>}/>
+        </Routes>
+      </main>
+    </div>
+  )
+}
 
 export function Guest() {
   return (
-    <div>
-      <h1>Guest</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Scafold />}/>
+      <Route path="/iniciar" element={<Login/>}/>
+      <Route path="*" element={<div><h1>No encontrado</h1></div>}/>
+    </Routes>
   )
 }
