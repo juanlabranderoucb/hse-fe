d925d9268592ae90ccc04e226987d69b7a27fa74
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-25 20:29:41 -0400

    chore: providers integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { AuthProvider } from './hooks/auth/auth.provider.tsx'
 import { App } from './app/app.tsx'
+import { FetchProvider } from './hooks/fetch/fetch.provider.tsx'
+
+const client = {
+  url: import.meta.env.VITE_API_URI,
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FetchProvider client={client}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </FetchProvider>
+    
   </StrictMode>,
 )
