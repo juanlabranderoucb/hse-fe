50cf1ac3b9bfdcfaa95d1f598d41548b56741ccb
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-27 18:22:21 -0400

    feat: nuevas propiedades del reporte

--- src/modules/safety/rcs/sc-report.type.ts
+++ src/modules/safety/rcs/sc-report.type.ts
@@ -1,12 +1,24 @@
+export interface ScReportImpact {
+  id: number;
+  description: string;
+}
+
+interface ScReportSuggestedFix {
+  id: number;
+  description: string;
+}
 
 export interface ScReport {
-  id: string;
+  id: number;
   description: string;
+  location: string;
   user: {
     userName: string;
     displayName: string;
   };
   elapsed: string;
+  impacts: Array<ScReportImpact>
+  suggestedFixes: Array<ScReportSuggestedFix>
   likes: number;
   replies: number;
 }
